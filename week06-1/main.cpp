4acc6c6d448b9923a0c9e82dc7fb03a33a492329
pei2830 <0101kathy@gmail.com>
2023-03-23 16:26:16 +0800

    week06

---
+++ week06-1/main.cpp
@@ -0,0 +1,37 @@
+#include <GL/glut.h>
+float angle = 0;
+void myCube()///改函式
+{
+    glPushMatrix();
+        glScalef(1, 0.3, 0.3);///調大小
+        glutSolidCube(0.5);///正方塊
+    glPopMatrix();
+}
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    glutSolidSphere(0.01, 30, 30);///小球做中心點參考
+
+    glPushMatrix();
+        ///glTranslatef(0.5, 0.5, 0.5);///這行是要決定掛在哪裡
+        ///glRotatef(angle, 0, 0, 1);
+        ///glTranslatef(0.45, 0, 0);
+        myCube();
+    glPopMatrix();
+
+
+
+	glutSwapBuffers();
+	angle++;
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week05");
+
+	glutDisplayFunc(display);
+    glutIdleFunc(display);
+	glutMainLoop();
+}
---
+++ week06-3/main.cpp
@@ -0,0 +1,59 @@
+#include <GL/glut.h>
+float angle = 0;
+void myCube()///改函式
+{
+    glPushMatrix();
+        glScalef(1, 0.3, 0.3);///調大小
+        glutSolidCube(0.5);///正方塊
+    glPopMatrix();
+}
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    glutSolidSphere(0.01, 30, 30);///小球做中心點參考
+
+    glPushMatrix();
+        glTranslatef(0.25, 0, 0);///這行是要決定掛在哪裡
+        glRotatef(angle, 0, 0, 1);
+        glTranslatef(0.25, 0, 0);
+        myCube();
+    glPushMatrix();
+        glTranslatef(0.25, 0, 0);///這行是要決定掛在哪裡
+        glRotatef(angle, 0, 0, 1);
+        glTranslatef(0.25, 0, 0);
+        myCube();
+        glPopMatrix();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(-0.25, 0, 0);///這行是要決定掛在哪裡
+        glRotatef(-angle, 0, 0, 1);
+        glTranslatef(-0.25, 0, 0);
+        myCube();
+    glPushMatrix();
+        glTranslatef(-0.25, 0, 0);///這行是要決定掛在哪裡
+        glRotatef(-angle, 0, 0, 1);
+        glTranslatef(-0.25, 0, 0);
+        myCube();
+        glPopMatrix();
+    glPopMatrix();
+    glutSwapBuffers();
+	///angle++;//把 angle++註解掉
+}
+void motion(int x, int y)
+{///新加的 mouse motion
+    angle = x;
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week05");
+
+    glutMotionFunc(motion);///新加的 mouse motion
+	glutDisplayFunc(display);
+    glutIdleFunc(display);
+
+	glutMainLoop();
+}
