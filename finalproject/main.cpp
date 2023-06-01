44af0c57ad88b6aa5ad952d52e8184c697aa73bf
pei28 <0101kathy@gmail>
2023-06-01 16:42:19 +0800

    week17

--- finalproject/main.cpp
+++ finalproject/main.cpp
@@ -4,8 +4,10 @@
 
 GLMmodel * head = NULL;
 GLMmodel * body = NULL;
-GLMmodel * uparm = NULL;
-GLMmodel * downarm = NULL;
+GLMmodel * leftarm = NULL;
+GLMmodel * rightarm = NULL;
+GLMmodel * leftfeet = NULL;
+GLMmodel * rightfeet = NULL;
 int show[4] = {1,1,1,1};
 int ID = 3;
 float teapotX =0, teapotY = 0;
@@ -21,52 +23,64 @@ void keyboard(unsigned char key, int x,int y) {
 }
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    if(head==NULL){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    if (head == NULL) {
         head = glmReadOBJ("model/head.obj");
         body = glmReadOBJ("model/body.obj");
-        uparm = glmReadOBJ("model/uparm.obj");
-        downarm = glmReadOBJ("model/downarm.obj");
-
+        leftarm = glmReadOBJ("model/leftarm.obj");
+        rightarm = glmReadOBJ("model/rightarm.obj");
+        leftfeet = glmReadOBJ("model/leftfeet.obj");
+        rightfeet = glmReadOBJ("model/rightfeet.obj");
     }
     glPushMatrix();
-        glScalef(0.3, 0.3, 0.3);
-        glPushMatrix();
-            ///glTranslatef(teapotX, teapotY, 0);
+    glScalef(0.3, 0.3, 0.3);
 
-            if(ID==0) glColor3f(1,0,0);
-            else glColor3f(1,1,1);
-            if(show[0])glmDraw(head, GLM_MATERIAL);
-        glPopMatrix();
-
-        if(ID==1) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[1])glmDraw(body, GLM_MATERIAL);
-
-        glPushMatrix();
-            glTranslatef(-1.360000, +0.360000, 0);
-            glRotatef(angle[2], 0, 0, 1);
-            glTranslatef(1.360000, -0.360000, 0);
-
-        if(ID==2) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[2])glmDraw(uparm, GLM_MATERIAL);
-
-        glPushMatrix();
-            glTranslatef(-1.360000, +0.500000, 0);
-            glRotatef(angle[3], 0, 0, 1);
-            glTranslatef(1.360000, -0.500000, 0);
-
-        if(ID==3) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[3])glmDraw(downarm, GLM_MATERIAL);
+    glPushMatrix();
+    if (ID == 0)
+        glColor3f(1, 0, 0);
+    else
+        glColor3f(1, 1, 1);
+    if (show[0])
+        glmDraw(head, GLM_MATERIAL);
     glPopMatrix();
-glPopMatrix();
 
+    if (ID == 1)
+        glColor3f(1, 0, 0);
+    else
+        glColor3f(1, 1, 1);
+    if (show[1])
+        glmDraw(body, GLM_MATERIAL);
 
+    glPushMatrix();
+    glTranslatef(-1.36, 0.36, 0);
+    glRotatef(angle[2], 0, 0, 1);
+    glTranslatef(1.36, -0.36, 0);
+    if (ID == 2)
+        glColor3f(1, 0, 0);
+    else
+        glColor3f(1, 1, 1);
+    if (show[2])
+        glmDraw(leftarm, GLM_MATERIAL);
+
+    glPushMatrix();
+    glTranslatef(-1.36, 0.5, 0);
+    glRotatef(angle[3], 0, 0, 1);
+    glTranslatef(1.36, -0.5, 0);
+    if (ID == 3)
+        glColor3f(1, 0, 0);
+    else
+        glColor3f(1, 1, 1);
+    if (show[3])
+        glmDraw(rightarm, GLM_MATERIAL);
     glPopMatrix();
-    glColor3f(0,1,0);
-    glutSolidTeapot( 0.02 );
+    glPopMatrix();
+
+    glColor3f(0, 1, 0);
+    glPushMatrix();
+    glTranslatef(teapotX, teapotY, 0);
+    glutSolidTeapot(0.02);
+    glPopMatrix();
+
     glutSwapBuffers();
 }
 int oldX=0, oldY=0;
